809dd9c005c091e86780bbb877a6d937b61ba3a1
obiew <1779174194@qq.com>
2023-01-09 09:31:04 +0800

    Revert "fifth commit"
    This reverts commit 5ffad747

--- src/main/java/com/lwb/GitTest.java
+++ src/main/java/com/lwb/GitTest.java
@@ -4,5 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("git2");
+        System.out.println("git3");
+        System.out.println("git4");
+        System.out.println("git5");
     }
 }
