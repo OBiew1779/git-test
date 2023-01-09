340dde65a4a25326a877969060a1ecd72bc49553
obiew <1779174194@qq.com>
2023-01-09 09:29:22 +0800

    Revert "first commit"
    This reverts commit a3fa4565

--- src/main/java/com/lwb/GitTest.java
+++ src/main/java/com/lwb/GitTest.java
@@ -4,8 +4,5 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git");
         System.out.println("git2");
-        System.out.println("git3");
-        System.out.println("git4");
-        System.out.println("git5");
     }
 }
